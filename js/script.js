1b2dc3e418416c4ef1858f0f2a7e3c9cf6bb6a7f
Thiha Kyaw Zin <thiha794411@gmail.com>
2025-10-01 21:03:25 +0630

    add_contact_form_and_fix_ui_2025_10_01

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function buildCards(dataList, container) {
-    return dataList.forEach(data => {
-        const card = document.createElement('div');
-        card.className = 'card';
+    return dataList.forEach((data) => {
+        const card = document.createElement("div");
+        card.className = "card";
         card.innerHTML = `
             <div class="card-header">
                 <div class="card-header-left">
@@ -18,15 +18,17 @@ function buildCards(dataList, container) {
         `;
         container.appendChild(card);
     });
-};
+}
 
-const texts = ["Backend Developer (PHP & Laravel)", "Mobile Developer (Flutter)"];
+const texts = [
+    "Backend Developer (PHP & Laravel)",
+    "Mobile Developer (Flutter)",
+];
 let index = 0;
 let charIndex = 0;
 let currentText = "";
 let isDeleting = false;
 const changingText = document.getElementById("changing-text");
-
 
 // Typewriter effect function
 function typeEffect() {
@@ -57,48 +59,87 @@ function typeEffect() {
 
 typeEffect();
 
-// Change navbar action effect
-const navbarItems = document.querySelectorAll('.navbar-item');
-navbarItems.forEach(item => {
-    item.addEventListener('click', () => {
-        document.querySelector('.navbar-item.active').classList.remove('active');
-        item.classList.add('active');
+const sections = document.querySelectorAll(".section");
+const navItems = document.querySelectorAll(".navbar-list a");
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.7,
+    delay: 100,
+    trackVisibility: true,
+};
+
+const observerCallback = (entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            const currentId = entry.target.getAttribute("id");
+            navItems.forEach((item) => {
+                item.classList.remove("active");
+            });
+
+            const activeNavItem = document.querySelector(`a[href="#${currentId}"]`);
+            if (activeNavItem) {
+                activeNavItem.classList.add("active");
+            }
+        }
+    });
+};
+
+const observer = new IntersectionObserver(observerCallback, options);
+sections.forEach((section) => {
+    observer.observe(section);
+});
+
+navItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        document.querySelector(".navbar-list a.active").classList.remove("active");
+        item.classList.add("active");
     });
 });
 
 // Add Experience and Education Section Dynamically
-const experiences = [
+const experiences = [{
+        title: "Web Developer (Laravel)",
+        sub_title: "CGM Golden Land",
+        duration: "2024 - Present",
+        description: "Worked on developing and maintaining web applications using PHP and the Laravel framework. Responsibilities included building new features, maintaining existing sites, integrating APIs, managing databases, and implementing server-side logic. Also collaborated on mobile application development using Flutter.",
+    },
     {
         title: "Web Developer (Laravel)",
         sub_title: "CGM Golden Land",
         duration: "2024 - Present",
-        description: "Worked on developing and maintaining web applications using PHP and the Laravel framework. Responsibilities included building new features, maintaining existing sites, integrating APIs, managing databases, and implementing server-side logic. Also collaborated on mobile application development using Flutter."
+        description: "Worked on developing and maintaining web applications using PHP and the Laravel framework. Responsibilities included building new features, maintaining existing sites, integrating APIs, managing databases, and implementing server-side logic. Also collaborated on mobile application development using Flutter.",
+    },
+    {
+        title: "Web Developer (Laravel)",
+        sub_title: "CGM Golden Land",
+        duration: "2024 - Present",
+        description: "Worked on developing and maintaining web applications using PHP and the Laravel framework. Responsibilities included building new features, maintaining existing sites, integrating APIs, managing databases, and implementing server-side logic. Also collaborated on mobile application development using Flutter.",
     },
 ];
 
-const educations = [
-    {
+const educations = [{
         title: "BSc in Mathematics",
         sub_title: "West Yangon University, Myanmar",
         duration: "2017 - 2024",
-        description: "I was enrolled in a Bachelor's degree program in Mathematics at West Yangon University. My studies encompassed various mathematical disciplines, including calculus, algebra, statistics, and applied mathematics. This academic experience provided me with a strong foundation in analytical thinking and problem-solving skills."
+        description: "I was enrolled in a Bachelor's degree program in Mathematics at West Yangon University. My studies encompassed various mathematical disciplines, including calculus, algebra, statistics, and applied mathematics. This academic experience provided me with a strong foundation in analytical thinking and problem-solving skills.",
     },
     {
         title: "Diploma in Software Engineering",
         sub_title: "Winner Computer Group, Myanmar",
         duration: "2018 - 2019",
-        description: "I was enrolled in a Bachelor's degree program in Mathematics at West Yangon University. My studies encompassed various mathematical disciplines, including calculus, algebra, statistics, and applied mathematics. This academic experience provided me with a strong foundation in analytical thinking and problem-solving skills."
+        description: "I was enrolled in a Bachelor's degree program in Mathematics at West Yangon University. My studies encompassed various mathematical disciplines, including calculus, algebra, statistics, and applied mathematics. This academic experience provided me with a strong foundation in analytical thinking and problem-solving skills.",
     },
     {
         title: "Web Developer (Laravel) Certification",
         sub_title: "CodeLab, Myanmar",
         duration: "2022 - 2023",
-        description: "After finishing my high school education, I was learning Software Engineering at Winner Computer Group."
+        description: "After finishing my high school education, I was learning Software Engineering at Winner Computer Group.",
     },
 ];
 
-buildCards(experiences, document.getElementById('experience-body'));
-buildCards(educations, document.getElementById('education-body'));
+buildCards(experiences, document.getElementById("experience-body"));
+buildCards(educations, document.getElementById("education-body"));
 
 // Calculate experience duration
 function calculateExperience(experiences) {
@@ -107,34 +148,36 @@ function calculateExperience(experiences) {
     return experiences.reduce((total, exp) => {
         let [startYear, endYear] = exp.duration.split(" - ");
         startYear = parseInt(startYear);
-        endYear = (endYear === "Present") ? currentYear : parseInt(endYear);
+        endYear = endYear === "Present" ? currentYear : parseInt(endYear);
 
         return total + (endYear - startYear);
     }, 0);
 }
 
-document.getElementById('experience-duration').innerHTML = `${calculateExperience(experiences)}+<span>Years of Experience</span>`;
+document.getElementById(
+    "experience-duration"
+).innerHTML = `${calculateExperience(
+  experiences
+)}+<span>Years of Experience</span>`;
 
 // Add Project Section Dynamically
-const projects = [
-    {
-        title: 'Real Time WeatherProject',
-        description: 'This is a real time weather website, using OpenWeatherMap API.',
-        imageUrl: '/assets/img/weather-img.png',
-        projectUrl: 'https://thiha-kyawzin.github.io/Weather/',
-        externalUrl: 'https://openweathermap.org/api',
-        externalText: 'OpenWeatherMap API',
-    },
-]
+const projects = [{
+    title: "Real Time WeatherProject",
+    description: "This is a real time weather website, using OpenWeatherMap API.",
+    imageUrl: "/assets/img/weather-img.png",
+    projectUrl: "https://thiha-kyawzin.github.io/Weather/",
+    externalUrl: "https://openweathermap.org/api",
+    externalText: "OpenWeatherMap API",
+}, ];
 
-const projectContainer = document.getElementById('project-body');
-const footer = projectContainer.querySelector('.project-body-footer');
+const projectContainer = document.getElementById("project-body");
+const footer = projectContainer.querySelector(".project-body-footer");
 let indexCount = 0;
 
 projects.forEach((proj, indexCount) => {
     indexCount++;
-    const card = document.createElement('div');
-    card.className = 'project-body-card';
+    const card = document.createElement("div");
+    card.className = "project-body-card";
 
     const description = proj.externalUrl ?
         proj.description.replace(
@@ -160,24 +203,183 @@ projects.forEach((proj, indexCount) => {
 
     projectContainer.appendChild(card);
     if (indexCount > 2) {
-        card.classList.add('hide');
+        projectContainer.appendChild(footer);
+        card.classList.add("hide");
+    } else {
+        if (projectContainer.contains(footer)) {
+            projectContainer.removeChild(footer);
+        }
     }
 
-    const arrowIcon = card.querySelector('.icon-arrow');
-    card.addEventListener('mouseenter', () => {
-        arrowIcon.classList.add('clicked');
-        arrowIcon.addEventListener('click', () => {
-            setTimeout(() => window.open(proj.projectUrl, '_blank'), 100);
+    const arrowIcon = card.querySelector(".icon-arrow");
+    card.addEventListener("mouseenter", () => {
+        arrowIcon.classList.add("clicked");
+        arrowIcon.addEventListener("click", () => {
+            setTimeout(() => window.open(proj.projectUrl, "_blank"), 100);
         });
     });
-    card.addEventListener('mouseleave', () => {
-        arrowIcon.classList.remove('clicked');
+    card.addEventListener("mouseleave", () => {
+        arrowIcon.classList.remove("clicked");
     });
 });
-projectContainer.appendChild(footer);
 
-footer.addEventListener('click', () => {
-    const hiddenCards = projectContainer.querySelectorAll('.project-body-card.hide');    
-    hiddenCards.forEach(card => card.classList.remove('hide'));
-    footer.style.display = 'none';
+footer.addEventListener("click", () => {
+    const hiddenCards = projectContainer.querySelectorAll(
+        ".project-body-card.hide"
+    );
+    hiddenCards.forEach((card) => card.classList.remove("hide"));
+    footer.style.display = "none";
+});
+
+// Contact Form Submission
+const contactForm = document.getElementById("contact-form");
+const patterns = {
+    name: /^[a-zA-Z\s'-]+$/,
+    email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    phone: /^\+?[0-9]+$/,
+};
+
+const messages = {
+    success: {
+        header: "Success",
+        content: "Your message has been sent successfully!",
+    },
+    error: {
+        header: "Error",
+        content: "There was an error sending your message. Please try again later.",
+    },
+};
+
+const popupOverlay = document.getElementById("popup");
+const closeButton = popupOverlay.querySelector(".close");
+const body = document.body;
+closeButton.addEventListener("click", hidePopup);
+
+const serviceId = "service_vz2tvof";
+const templateId = "template_1clojs9";
+const publicKey = "UseHS2qgqrZHSoiSS";
+
+// Initialize EmailJS
+emailjs.init(publicKey);
+
+contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let isValid = true;
+
+    contactForm.querySelectorAll(".form-group").forEach((group) => {
+        const input = group.querySelector(".input-field");
+        const error = group.querySelector(".error");
+        error.textContent = "";
+        let message = "";
+
+        if (!required(input.value)) {
+            message = `This ${input.name} is required.`;
+            isValid = false;
+        } else if (!minLength(input.value, getMinLength(input.name))) {
+            const min = getMinLength(input.name);
+            message = `This ${input.name} must be at least ${min} characters.`;
+            isValid = false;
+        } else if (!maxLength(input.value, getMaxLength(input.name))) {
+            const max = getMaxLength(input.name);
+            message = `This ${input.name} must be less than ${max} characters.`;
+            isValid = false;
+        } else if (
+            patterns[input.name] &&
+            !validateFormat(input.value, patterns[input.name])
+        ) {
+            switch (input.name) {
+                case "name":
+                    message = `Please use A-Z, a-z, space, ', - only`;
+                    break;
+                case "email":
+                    message = `This email format is invalid.`;
+                    break;
+                case "phone":
+                    message = `Please use 0-9 and + only`;
+                    break;
+                default:
+                    `This ${input.name} format is invalid.`;
+                    break;
+            }
+            isValid = false;
+        }
+
+        if (message) {
+            error.textContent = message;
+        }
+    });
+
+    if (isValid) {
+        emailjs
+            .sendForm(serviceId, templateId, contactForm)
+            .then(() => {
+                contactForm.reset();
+                showPopup(messages.success);
+            })
+            .catch((error) => {
+                showPopup(messages.error);
+                console.error("Error sending email:", error);
+            });
+    }
+});
+
+function required(value) {
+    return value && value.trim().length > 0;
+}
+
+function minLength(value, min) {
+    return value.length >= min;
+}
+
+function maxLength(value, max) {
+    return value.length <= max;
+}
+
+function getMinLength(fieldName) {
+    switch (fieldName) {
+        case "name":
+            return 2;
+        case "phone":
+            return 7;
+        default:
+            return 0;
+    }
+}
+
+function getMaxLength(fieldName) {
+    switch (fieldName) {
+        case "name":
+            return 125;
+        case "email":
+            return 125;
+        case "phone":
+            return 20;
+        case "message":
+            return 500;
+    }
+}
+
+function validateFormat(value, pattern) {
+    return pattern.test(value);
+}
+
+function showPopup(message) {
+    const popup_header = popupOverlay.querySelector(".popup-header");
+    const popup_content = popupOverlay.querySelector(".popup-content");
+    popup_header.textContent = message.header;
+    popup_content.textContent = message.content;
+
+    popupOverlay.classList.add("show");
+    body.classList.add("popup-open");
+}
+
+function hidePopup() {
+    popupOverlay.classList.remove("show");
+    body.classList.remove("popup-open");
+}
+
+popupOverlay.addEventListener("click", function (event) {
+    if (event.target === popupOverlay) {
+        hidePopup();
+    }
 });
